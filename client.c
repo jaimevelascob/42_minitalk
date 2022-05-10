1555180fb0fc709c5b63217efce6810070dfd43f
jaimevelascob <jaimevelasco3@hotmail.com>
2022-05-10 18:03:51 +0200

    fixing unicode

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: jvelasco <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 16:36:33 by jvelasco          #+#    #+#             */
-/*   Updated: 2022/05/06 14:28:26 by jvelasco         ###   ########.fr       */
+/*   Updated: 2022/05/10 18:03:18 by jvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,44 +18,45 @@ int			ft_atoi(const char *str);
 int			ft_strlen(const char *s);
 static int	g_msg;
 
-void    msg_received()
+void	msg_received()
 {
 	static int	x = 1;
-   
+
 	if (x == g_msg * 8)
-        printf("msg received %d/%d\n", x, g_msg * 8);
+		printf("msg received %d/%d\n", x, g_msg * 8);
 	x++;
+	usleep(50);
 }
 
 void	call(char **argv, int server_pid)
 {
-    int	j;
-	
-	while(*argv[2])
-	{ 
-	   j = 7;
-	   while (j >= 0)
-	   {
-			if(*argv[2] & (1 << j))
-			  kill(server_pid, SIGUSR2);
+	int	j;
+
+	while (*argv[2])
+	{
+		j = 7;
+		while (j >= 0)
+		{
+			if (*argv[2] & (1 << j))
+				kill(server_pid, SIGUSR2);
 			else
-			  kill(server_pid, SIGUSR1);
-			j--; 
+				kill(server_pid, SIGUSR1);
+			j--;
 			usleep(180);
-	   }
-	   argv[2]++;
+		}
+		argv[2]++;
 	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    const int	server_pid = ft_atoi(argv[1]);
-    
-    signal(SIGUSR1, msg_received);
+	const int	server_pid = ft_atoi(argv[1]);
+
+	signal(SIGUSR1, msg_received);
 	if (argc <= 2)
-		printf("need more parametres");
+		printf("need parametres");
 	else if (server_pid == 0 || (kill(server_pid, SIGUSR1) == -1
-		&& kill(server_pid, SIGUSR2) == -1))
+			&& kill(server_pid, SIGUSR2) == -1))
 		printf("pid dosnt exist");
 	else
 	{
--- server.c
+++ server.c
@@ -2,36 +2,34 @@
 #include <unistd.h> 
 #include <signal.h>
 
-void    handler(int sig, siginfo_t *info, void *ucontext)
+void	handler(int sig, siginfo_t *info, void *ucontext)
 {
-    static char x;
-    static int  j = 7;
+	static unsigned char	x;
+	static int	j = 7;
 
-    (void)(ucontext);
-    if (sig == SIGUSR2)
-	{
-        x = x | (1 << j);
-	}
-	kill(info->si_pid, SIGUSR1);
+	(void)(ucontext);
+	if (sig == SIGUSR2)
+		x = x | (1 << j);
 	if (j == 0)
-	{ 
-        write(1, &x, 1);
-        x = 0;
-        j = 7;
-    }
-    else
-        j--;
+	{
+		write(1, &x, 1);
+		x = 0;
+		j = 7;
+	}
+	else
+		j--;
+	kill(info->si_pid, SIGUSR1);
 }
 
-int main(void)
+int	main(void)
 {
-    struct sigaction    sa;
+	struct sigaction	sa;
 
-    sa.sa_sigaction = handler;
-    sa.sa_flags = SA_SIGINFO;
-    printf("%d\n", getpid());
-    sigaction(SIGUSR1, &sa, 0);
-    sigaction(SIGUSR2, &sa, 0);
-    while(1)
-        pause();
+	sa.sa_sigaction = handler;
+	sa.sa_flags = SA_SIGINFO;
+	printf("%d\n", getpid());
+	sigaction(SIGUSR1, &sa, 0);
+	sigaction(SIGUSR2, &sa, 0);
+	while (1)
+		pause();
 }
